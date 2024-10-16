b8718d409234e3685e282eeab5377b5a1e299225
Anveshika <anveshika.srivastava@epsilonium.com>
2024-10-16 23:09:32 +0530

    fixed

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const corsOptions = {
   };
   
   app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
 // app.use(cors({ origin: ["https://ashugallery12.herokuapp.com"], 
 
 // methods:["GET","POST","DELETE","PUT"],
