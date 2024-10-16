34587ac8c80fb512cc7b7e34d244785e1f91cd19
Anveshika <anveshika.srivastava@epsilonium.com>
2024-10-16 22:12:38 +0530

    fixed

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
 require("./config/config");
 require("./model/db");
-const compression = require('compression')
+const compression = require('compression');
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const { links } = require("express/lib/response");
 
-var app = express()
-app.use(compression())
-app.use(cors({
-    origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Allows cookies to be sent along with the request
-  }));
+const app = express();
+app.use(compression());
+
+const corsOptions = {
+    origin: 'http://localhost:4200', // Your Angular app's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allows credentials to be sent
+};
+
+app.use(cors(corsOptions));
+
+// Handle preflight requests
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
-  }
 });
+
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
-app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-// app.use(cors({ origin: ["https://ashugallery12.herokuapp.com"], 
 
-// methods:["GET","POST","DELETE","PUT"],
-// origin:true,
-// credentials: true }))
-
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 app.use(require('./routes/index.route'));
-app.use((err,req,res,next)=>{
-    if(err.name === 'ValidationError'){
-            var valError =[];
-            Object.keys(err.errors).forEach(key => valError.push(err.errors[key].message));
-            res.status(422).send(valError)
-    }
-})
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static("client/www"))
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        const valError = [];
+        Object.keys(err.errors).forEach(key => valError.push(err.errors[key].message));
+        return res.status(422).send(valError);
+    }
+});
+
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/www"));
     const path = require("path");
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','www','index.html'))
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'www', 'index.html'));
+    });
 }
 
-app.listen(PORT,()=> console.log(`server started at port :${PORT}`))
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
