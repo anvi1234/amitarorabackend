d9659e6a7ca0c83d050ca9c4718da964ab156fcc
Anveshika <anveshika.srivastava@epsilonium.com>
2024-11-05 19:42:18 +0530

    fixed issue

--- controller/banner.controller.js
+++ controller/banner.controller.js
@@ -29,6 +29,22 @@ module.exports.getBanners = (req, res, next) => {
     });
 };
 
+module.exports.updateBanner = (req, res, next) => {
+    const { id } = req.params; // Assuming the ID of the banner is provided in the route parameters
+    const updateData = req.body; // Data to update from the request body
+
+    Banner.findByIdAndUpdate(id, updateData, { new: true }, (err, updatedBanner) => {
+        if (err) {
+            console.log(err);
+            return next(err);
+        } else if (!updatedBanner) {
+            res.status(404).json({ message: "Banner not found" });
+        } else {
+            res.json(updatedBanner);
+        }
+    });
+};
+
 // Delete a banner by ID
 module.exports.deleteBanner = (req, res, next) => {
     const id = req.params.id;
